c0f96af48766fc3f8d15e2dd54d8c392fb14fbb7
prokushev <yuri.prokushev@gmail.com>
2022-04-03 14:24:57 +0700

    Slightly better argument parsing. Better @Exit and END_PROCESS macros.

--- test.asm
+++ test.asm
@@ -1,7 +1,8 @@
+INCLUDE	BIOS.INC
 INCLUDE	DOS.INC
 
 _DATA	SEGMENT 
-HelloStr	DB	"Hello$"
+HelloStr	DB	"Hello",0dh,0ah,"$"
 pHelloStr	DW	HelloStr
 fpHelloStr	DD	HelloStr
 _DATA	ENDS
@@ -13,19 +14,34 @@ _STACK	ENDS
 _TEXT	SEGMENT 
 START:
 	MOV	AX, _DATA
-	MOV	ES, AX
+	MOV	DS, AX
 	ASSUME DS:_DATA
+
+	@Cls
 
 	; MASM 5.x style
 	@DispStr HelloStr
 	@DispStr pHelloStr
 	@DispStr fpHelloStr
+	MOV	AX, OFFSET HelloStr
+	@DispStr AX
+	MOV	DX, OFFSET HelloStr
+	@DispStr DX
 
 	; MASM 6.x style
 	DISPLAY HelloStr
 	DISPLAY pHelloStr
 	DISPLAY fpHelloStr
+	MOV	AX, OFFSET HelloStr
+	DISPLAY	AX
+	MOV	DX, OFFSET HelloStr
+	DISPLAY	DX
 
+	; MASM 5.x style
+	@Exit	0
+
+	; MASM 6.x style
+	END_PROCESS 0
 _TEXT	ENDS
 
 	END	START
